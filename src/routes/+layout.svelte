6162282fbfdb5b463990ced55a67eee8e673e482
Robison Rodrigues dos Santos <rwrsantos@gmail.com>
2026-06-12 14:39:12 -0700

    feat: add CSS View Transitions API for page navigation
    - Replaced Svelte fly transition with native @view-transition
    - Added custom fade/slide animations (300ms ease-out)
    - Works for same-origin navigation without JavaScript overhead

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,9 @@
 <script lang="ts">
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
-	import { fly } from 'svelte/transition';
 
 	let { children } = $props();
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
-<div in:fly={{ y: 20, duration: 300 }} out:fly={{ y: -20, duration: 200 }}>
-	{@render children()}
-</div>
+{@render children()}
